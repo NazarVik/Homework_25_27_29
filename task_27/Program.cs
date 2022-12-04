43882da579868c71a5de50749a4b86db135f251c
NazarVik <Nazarov_dev@icloud.com>
2022-12-04 12:55:10 +0300

    27 задача решена

--- task_27/Program.cs
+++ task_27/Program.cs
@@ -2,3 +2,29 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number  = int.Parse(Console.ReadLine());
+    return number;
+}
+ 
+int SumOfNumbers(int num)
+{
+    int result = 0;
+    int length = num.ToString().Length;
+    for(int i =0; i < length; i++)
+    {
+        result += num % 10;
+        num = num / 10;        
+    }
+    return result;
+}
+
+Console.Clear();
+
+int number = DataEntry("введите число  = ");
+int res = SumOfNumbers(number);
+
+Console.WriteLine($"{number} -> {res}");
--- task_29/Program.cs
+++ task_29/Program.cs
@@ -1,3 +1,11 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number  = int.Parse(Console.ReadLine());
+    return number;
+}
